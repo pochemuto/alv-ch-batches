121198549f11c785b7fcc3ab04217f40d53daaba
Gilles Cornu <git@gilles.cornu.name>
2016-06-27 13:55:29 +0200

    [Minor] partnerjob: refactor and clean a little bit

--- master/partnerjob/src/main/java/ch/alv/batches/partnerjob/to/master/batch/ProspectiveJobToPartnerJobConverter.java
+++ master/partnerjob/src/main/java/ch/alv/batches/partnerjob/to/master/batch/ProspectiveJobToPartnerJobConverter.java
@@ -22,6 +22,7 @@ public class ProspectiveJobToPartnerJobConverter implements ItemProcessor<Prospe
     // TODO: convert these constants as configurable parameters
     public static final int DESC_MAX_LENGTH = 10000;
     public static final String DESC_TRUNCATE_SUFFIX = " [...]";
+    private static final SimpleDateFormat AVAM_DATETIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd'-00.00.00.000000'");
 
     private String partnerCode = null;
 
@@ -35,8 +36,23 @@ public class ProspectiveJobToPartnerJobConverter implements ItemProcessor<Prospe
     public OstePartnerRecord process(ProspectiveJob prospectiveJob) throws Exception {
 
         OstePartnerRecord partnerJob = new OstePartnerRecord();
+        partnerJob.setId(UUID.randomUUID().toString());
         partnerJob.setQuelle(partnerCode);
         partnerJob.setBezeichnung(prospectiveJob.getStellentitel().trim());
+        processBeschreibung(prospectiveJob, partnerJob);
+        partnerJob.setUntName(prospectiveJob.getKundenname().trim());
+        partnerJob.setBerufsgruppe((long) prospectiveJob.getMetadaten().getJobcategory()); // FIXME switch to Integer in jOOQ
+        partnerJob.setArbeitsortPlz(prospectiveJob.getMetadaten().getZipcode().trim());
+        partnerJob.setAnmeldeDatum(AVAM_DATETIME_FORMAT.format(prospectiveJob.getDatumStart().toGregorianCalendar().getTime()));
+        partnerJob.setPensumVon(prospectiveJob.getMetadaten().getPensumvon());
+        partnerJob.setPensumBis(prospectiveJob.getMetadaten().getPensumbis());
+        partnerJob.setUrlDetail(prospectiveJob.getUrlDirektlink().trim());
+        // not available in legacy database: partnerJob.setSprache(prospectiveJob.getSprache().trim());
+
+        return partnerJob;
+    }
+
+    private void processBeschreibung(ProspectiveJob prospectiveJob, OstePartnerRecord partnerJob) {
         partnerJob.setBeschreibung(prospectiveJob.getTexte().getText1().trim() + "\n" +
                 prospectiveJob.getTexte().getText2().trim() + "\n" +
                 prospectiveJob.getTexte().getText3().trim() + "\n" +
@@ -53,17 +69,5 @@ public class ProspectiveJobToPartnerJobConverter implements ItemProcessor<Prospe
                     .substring(0, DESC_MAX_LENGTH - DESC_TRUNCATE_SUFFIX.length()) + DESC_TRUNCATE_SUFFIX);
         }
         partnerJob.setBeschreibung(partnerJob.getBeschreibung().trim());
-        partnerJob.setId(UUID.randomUUID().toString());
-        partnerJob.setUntName(prospectiveJob.getKundenname().trim());
-        partnerJob.setBerufsgruppe((long) prospectiveJob.getMetadaten().getJobcategory()); // FIXME switch to Integer in jOOQ
-        partnerJob.setArbeitsortPlz(prospectiveJob.getMetadaten().getZipcode().trim());
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'-00.00.00.000000'");
-        partnerJob.setAnmeldeDatum(sdf.format(prospectiveJob.getDatumStart().toGregorianCalendar().getTime()));
-        partnerJob.setPensumVon(prospectiveJob.getMetadaten().getPensumvon());
-        partnerJob.setPensumBis(prospectiveJob.getMetadaten().getPensumbis());
-        partnerJob.setUrlDetail(prospectiveJob.getUrlDirektlink().trim());
-        // not available in legacy database: partnerJob.setSprache(prospectiveJob.getSprache().trim());
-
-        return partnerJob;
     }
 }
