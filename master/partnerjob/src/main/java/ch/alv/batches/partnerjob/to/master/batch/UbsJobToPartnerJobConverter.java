6d97abffb5bc0456cdd9f4de68ec4bc0e4880639
Gilles Cornu <git@gilles.cornu.name>
2016-09-28 23:57:35 +0200

    checkstyle: Remove an unused import

--- master/partnerjob/src/main/java/ch/alv/batches/partnerjob/to/master/batch/UbsJobToPartnerJobConverter.java
+++ master/partnerjob/src/main/java/ch/alv/batches/partnerjob/to/master/batch/UbsJobToPartnerJobConverter.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Contains all logic that transforms a raw {@link ch.alv.batches.partnerjob.to.master.jaxb.ubs.Inserat} object into
